0ba6972adcea6388b6d5867cb93ad229b0e6ca2f
Cawan de Melo Santos <melocawanprofi@gmail.com>
2025-03-11 21:29:34 -0300

    Att 0.1

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") :
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() :
 		velocity.y = JUMP_FORCE
 		is_jumping = true
 	elif is_on_floor():
